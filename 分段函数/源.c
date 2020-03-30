66e726661876e8c782b87ee96d9afc943becd1b0
成 秦 <2548762202@qq.com>
2020-03-30 08:13:44 +0800

    添加项目文件。

---
+++ 分段函数/源.c
@@ -0,0 +1,23 @@
+#include "stdio.h"
+#include "math.h"
+int main()
+{
+    float x, y;
+    y = 0;
+    printf("«Î ‰»Îxµƒ÷µ:");
+    scanf_s("%f", &x);
+    if (x < 0 && x != -3)
+    {
+        y = x * x + x - 6;
+    }
+    else if (x >= 0 && x < 10 && x != 2 && x != 3)
+    {
+        y = x * x - 5 * x + 6;
+    }
+    else
+    {
+        y = x * x - x - 1;
+    }
+            printf("%f\n", y);
+        return 0;
+    }
